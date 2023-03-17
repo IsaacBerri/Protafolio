0fa15ec5629fa0ca287a08063d231759a76c628f
Isaac Berrio <berriisaax18@hotmail.com>
2023-03-17 15:49:39 -0500

    Seccion de proyectos terminada (aun sin parallax) y seccion de skills terminada

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
-const rellax = new Rellax (".rellax")
 AOS.init();
 
+// const parallax = document.querySelector(".paralax")
+// let _fron = parallax.getAttribute("data-from") || "0px"
+// let _to = parallax.getAttribute("data-to") || "100px"
+// let instance = basicScroll.create({
+//     elem: parallax,
+//     from: "top-middle",
+//     to: "bottom-middle",
+//     direct: true,
+//     props: {
+//         "-ty": {
+//             from: _fron,
+//             to: _to,
+//         }
+//     }
+
+// })
+// instance.start();
