6ae568e668c6c6ff3df2d8878111b00ee073233a
Isaac Berrio <berriisaax18@hotmail.com>
2023-03-20 18:39:22 -0500

    Terminado el media query de movile

--- js/main.js
+++ js/main.js
@@ -12,17 +12,17 @@ const cometa4 = document.querySelector(".cometa-4");
 const cometa5 = document.querySelector(".cometa-5");
 const cometa6 = document.querySelector(".cometa-6");
 
-function parallaxVertical(ele, multiplicador, puntoDeArranque) {
+function parallaxVertical(ele, multiplicador, puntoDeArranque, puntoFinal) {
   window.addEventListener("scroll", () => {
-    if (window.scrollY >= puntoDeArranque) {
+    if (window.scrollY >= puntoDeArranque & window.scrollY <= puntoFinal) {
         let value = window.scrollY;
         ele.style.marginTop = (value -  puntoDeArranque) * multiplicador + "px";
     }
   });
 }
-function parallaxMixto(ele, multiplicador, puntoDeArranque) {
+function parallaxMixto(ele, multiplicador, puntoDeArranque, puntoFinal) {
   window.addEventListener("scroll", () => {
-      if (window.scrollY >= puntoDeArranque) {
+      if (window.scrollY >= puntoDeArranque & window.scrollY <= puntoFinal) {
         let value = window.scrollY;
       ele.style.marginTop = (value - puntoDeArranque) * multiplicador + "px";
       ele.style.marginLeft = (value - puntoDeArranque) * multiplicador + "px";
@@ -30,16 +30,16 @@ function parallaxMixto(ele, multiplicador, puntoDeArranque) {
   });
 }
 
-parallaxVertical(text, 1.3, 0);
-parallaxVertical(capa1, 0.7, 0);
-parallaxVertical(capa2, 0.5, 0);
-parallaxVertical(seccionProyectos, 0.7, 780)
-parallaxVertical(seccionContacto, 1.4, 2560)
-parallaxMixto(cometa1, 0.6, 900);
-parallaxMixto(cometa2, 0.3, 1050);
-parallaxMixto(cometa3, 0.6, 900);
-parallaxMixto(cometa4, 0.2, 1150);
-parallaxMixto(cometa5, 0.8, 900);
-parallaxMixto(cometa6, 0.1, 950);
+parallaxVertical(text, 1.3, 0, 670);
+parallaxVertical(capa1, 0.7, 0, 670);
+parallaxVertical(capa2, 0.5, 0, 670);
+parallaxVertical(seccionProyectos, 0.7, 780, 2400)
+parallaxVertical(seccionContacto, 1.4, 2560, 5000)
+parallaxMixto(cometa1, 0.6, 900, 2300);
+parallaxMixto(cometa2, 0.3, 1050, 2300);
+parallaxMixto(cometa3, 0.6, 900, 1666);
+parallaxMixto(cometa4, 0.2, 1150, 1917);
+parallaxMixto(cometa5, 0.8, 900, 2450);
+parallaxMixto(cometa6, 0.1, 950, 1930);
 
 
